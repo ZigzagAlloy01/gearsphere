21e6296724f817287ad61d607fe2e811c54e502a
MichaelKazembe <michaelkazembe@gmail.com>
2026-09-14 21:29:25 +0200

    Added site Color Theme to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-2xl font-bold p-4">Welcome to GearSphere!</div>
+      <div className="bg-primary text-white p-4">GearSphere Primary Color</div>
+
+      <div className="bg-secondary text-white p-4">
+        GearSphere Secondary Color
+      </div>
+
+      <div className="bg-accent text-white p-4">GearSphere Accent Color</div>
     </main>
   );
 }
